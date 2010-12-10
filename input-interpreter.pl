f262f194f1a844f42ebb527e573bc4e44a873a31
Robert Schriver <schriver@rancor.csh.rit.edu>
2010-12-09 21:12:05 -0500

    Fixed function call

--- input-interpreter.pl
+++ input-interpreter.pl
@@ -24,7 +24,7 @@ foreach my $cell (@{$data->{cell}}){
 	push(@cells,\%current_cell);
 }
 
-sum_delta_weight(@profiles);
+sum_delta_weight(@profiles , @cells);
 print get_midpoint(@profiles);
 
 #print "Best delta value: $best_delta\nClosest x: $best_x\nClosest y: $best_y\n";
